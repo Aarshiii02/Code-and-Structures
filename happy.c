6221035d2ba82ea74da92594b42160d32701e362
Aarshi <aarshidadhania@gmail.com>
2025-08-17 14:20:19 +0530

    Add files via upload

---
+++ happy.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+int printAndSum(int n)
+{
+    int sum = 0,digits[20],count = 0;
+    while(n > 0)
+    {
+        digits[count++]= n % 10;
+        n/=10;
+    }
+    for (int i = count - 1;i >= 0;i--)
+    {
+        printf("(%d)^2",digits[i]);
+        sum += digits[i]*digits[i];
+        if (i > 0)
+        {
+            printf(" + ");
+        }
+    }
+    printf(" = %d\n", sum);
+
+    return sum;
+}
+int isHappy(int n)
+{
+    int seen[1000] = {0}; 
+
+    while (n != 1 && !seen[n])
+    {
+        seen[n] = 1;
+        n = printAndSum(n);
+    }
+
+    if (n == 1)
+    {
+        printf("Result: true (Happy Number)\n");
+        return 1;
+    }
+    else
+    {
+        printf("Result: false (Not Happy Number)\n");
+        return 0;
+    }
+    
+}
+
+int main()
+{
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    printf("proof/explanation: \n");
+    isHappy(n);
+    return 0;
+}
